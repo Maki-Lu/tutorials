27306186fe4edf1c8b53bb15b66c08e9175e3c9b
Maki-Lu <60336553+Maki-Lu@users.noreply.github.com>
2022-07-05 15:32:48 -0400

    1

--- src/maki's tutorials/016 逆否命题.lean
+++ src/maki's tutorials/016 逆否命题.lean
@@ -1,12 +1,38 @@
 import data.real.basic
 
-example (P Q: Prop) (pq: P → Q): (¬ Q → ¬ P) :=
+/- 严格证明法 -/
+example (P Q: Prop) (pq: P → Q): ¬ Q → ¬ P :=
 begin
-  intro p,
-  exact ⟨pq p, pr p⟩,
+  intros nq p,
+  exact nq (pq p),
 end
 
+example (P Q: Prop) (npnq: ¬ P → ¬ Q): Q → P :=
+begin
+  intro q,
+  by_contra np,
+  exact npnq np q,
+end
 
+/- 直接取逆否命题 -/
+example (P Q: Prop) (pq: P → Q): ¬ Q → ¬ P :=
+begin
+  contrapose,
+  push_neg,
+  exact pq,  
+end
+
+example (P Q: Prop) (pq: P → Q): ¬ Q → ¬ P :=
+begin
+  contrapose!,
+  exact pq,
+end
+
+example (P Q: Prop) (npnq: ¬ P → ¬ Q): Q → P :=
+begin
+  contrapose,
+  exact npnq,
+end
 
 
 
