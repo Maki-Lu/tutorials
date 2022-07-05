a3592ecef38e207a67c53f6a5d9b9296c2f0124e
Maki-Lu <60336553+Maki-Lu@users.noreply.github.com>
2022-07-05 09:16:38 -0400

    1

---
+++ src/maki's tutorials/002 直接证明.lean
@@ -0,0 +1,40 @@
+import data.real.basic
+
+/- 中间结论 -/
+example (P Q: Prop) (p: P) (pq: P → Q): Q :=
+begin
+  have:= pq p,
+  exact this,
+end
+
+example (P Q: Prop) (p: P) (pq: P → Q): Q :=
+begin
+  have q := pq p,
+  exact q,
+end
+
+example (P Q: Prop) (p: P) (pq: P → Q): Q :=
+begin
+  have q: Q,
+    exact pq p,
+  exact q,
+end
+
+example (P Q R: Prop) (p: P) (pq: P → Q) (qr: Q → R): R :=
+begin
+  have q := pq p,
+  have r := qr q,
+  exact r,
+end
+
+example (P Q R: Prop) (p: P) (pq: P → Q) (qr: Q → R): R :=
+begin
+  have q := pq p,
+  exact qr q,
+end
+
+example (P Q R: Prop) (p: P) (p_qr: P → (Q → R)) (q: Q): R :=
+begin
+  have qr := p_qr p,
+  exact qr q,
+end
