14ed721119303d258b4f567e3109a1c694c2ba7d
Maki-Lu <60336553+Maki-Lu@users.noreply.github.com>
2022-07-05 09:36:08 -0400

    1

--- src/maki's tutorials/001 条件证明.lean
+++ src/maki's tutorials/001 条件证明.lean
@@ -54,35 +54,7 @@ end
 
 
 
-/- 只须证明 -/
 
-example (P Q R: Prop) (p: P) (pq: P → Q) (qr: Q → R): R :=
-begin
-  suffices q: Q,
-    exact qr q,
-  exact pq p,
-end
-
-example (P Q R: Prop) (p: P) (pq: P → Q) (qr: Q → R): R :=
-begin
-  suffices q: Q,
-    exact qr q,
-  have q := pq p,
-  exact q,
-end
-
-/- 全称量词 -/
-example (P: ℝ → Prop): ∀x, (P x → P x) :=
-begin
-  intro x,
-  exact id,
-end
-
-example: ∀ x y: ℝ, x + y = y + x :=
-begin
-  intros x y,
-  by ring,
-end
 
 /- 存在量词 -/
 example: ∃ x : ℝ, 2*x = 4 :=
--- src/maki's tutorials/00?: Logical Operations.lean
+++ src/maki's tutorials/00?: Logical Operations.lean
@@ -1,17 +1,7 @@
 import data.real.basic
 
 
-/- 重写命题 -/
-/- 示例
-  mul_assoc a b c : a * b * c = a * (b * c)
-  mul_comm a b : a * b = b * a   -/
 
-example (x y z: ℝ): x * y * z = z * y * x :=
-begin
-  rw mul_comm (x * y ) z,
-  rw mul_comm x y,
-  rw mul_assoc,
-end
 
 /- 递等式证明 -/
 example (x y z: ℝ): x * y * z = z * y * x :=
