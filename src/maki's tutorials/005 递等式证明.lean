b70fdaba2afcb3f18c042605da331f7b685f44d1
Maki-Lu <60336553+Maki-Lu@users.noreply.github.com>
2022-07-05 13:06:49 -0400

    1

---
+++ src/maki's tutorials/005 递等式证明.lean
@@ -0,0 +1,15 @@
+import data.real.basic
+
+/- 递等式证明 -/
+example (x y z: ℝ): x * y * z = z * y * x :=
+begin
+  calc x * y * z = z * (x * y): by rw mul_comm
+    ... = z * (y * x): by rw mul_comm x y
+    ... = z * y * x: by rw mul_assoc,
+end
+
+example (x y z: ℝ): x * y * z = z * y * x :=
+begin
+  by ring,
+end
+
---
+++ src/maki's tutorials/012 .lean
@@ -0,0 +1,5 @@
+example (P Q: Prop) (Hyp: ¬ P ∨ Q): P → Q :=
+begin
+  cases Hyp with np q,
+  
+end
