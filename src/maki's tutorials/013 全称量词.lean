12b8e6060980945231c9655da5a7c3edde701abc
Maki-Lu <60336553+Maki-Lu@users.noreply.github.com>
2022-07-05 14:31:45 -0400

    1

--- src/maki's tutorials/013 全称量词.lean
+++ src/maki's tutorials/013 全称量词.lean
@@ -1,14 +1,36 @@
 import data.real.basic
 
-/- 全称量词 -/
-example (P: ℝ → Prop): ∀x, (P x → P x) :=
+/- 结论用了全称量词 -/
+example (P: ℝ → Prop): ∀ x, (P x → P x) :=
 begin
   intro x,
   exact id,
 end
 
-example: ∀ x y: ℝ, x + y = y + x :=
+example: ∀ x y : ℝ, x * y = y * x :=
 begin
   intros x y,
-  by ring,
+  rw mul_comm,
 end
+
+/- 条件用了全称量词 -/
+
+example (P: ℝ → ( ℝ → Prop)) (Pxy: ∀ x, ∀ y, P x y): P 2 3 :=
+begin
+  specialize Pxy 2 3,
+  exact Pxy,
+end
+
+example (P: ℝ → ( ℝ → Prop)) (P2y: ∀ y, P 2 y): P 2 3 :=
+begin
+  specialize P2y 3,
+  exact P2y,
+end
+
+example (P: ℝ → ( ℝ → Prop)) (Pxy: ∀ x, ∀ y, P x y): ∀ y, ∀ x, P x y :=
+begin
+  intros y x,
+  specialize Pxy x y,
+  exact Pxy,
+end
+
