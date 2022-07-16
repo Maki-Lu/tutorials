fb2d9c9ef62896298d749947afdbf554afdf9a32
Maki-Lu <60336553+Maki-Lu@users.noreply.github.com>
2022-07-15 23:32:42 -0400

    1

--- src/Maki's Playground/group.lean
+++ src/Maki's Playground/group.lean
@@ -10,10 +10,14 @@ def yx_xxy (A: set ℝ) := ∀ x y : A, y * x = x * x * y
   （乘法结合律） mul_assoc a b c : a * b * c = a * (b * c)
   （乘法交换律） mul_comm a b : a * b = b * a   -/
 
-example (A: set ℝ) (hyp: yx_xxy A) (x y: ℝ):  x*y=y*x := /-x * x * y * y * x * x * y * y = x * y :=-/
+
+
+/-instance : semigroup A          := by apply_instance-/
+
+example (A: set ℝ) (hyp: yx_xxy A) (x y: ℝ): (x ∈ A ∧ y ∈ A) →  y * x = x * x * y := /-x * x * y * y * x * x * y * y = x * y :=-/
 begin
-
-  yx_xxy
-
+  rintro ⟨hx, hy⟩,
+  rw yx_xxy 
+  
 end
 
