3d7ff339b5a08c98d5e75e4544b329ef7cb0386b
Maki-Lu <60336553+Maki-Lu@users.noreply.github.com>
2022-07-16 09:08:44 -0400

    1

--- src/Maki's Playground/group.lean
+++ src/Maki's Playground/group.lean
@@ -1,25 +1,55 @@
 import data.real.basic
 
-def yx_xxy (A: set ℝ) := ∀ x y : ℝ, (x ∈ A ∧ y ∈ A)→ x*y=y*x 
-
 /- 命题重写 -/
 /- 示例
-  （乘法结合律） mul_assoc a b c : a * b * c = a * (b * c)
-  （乘法交换律） mul_comm a b : a * b = b * a   -/
-
-
-
-/-instance : semigroup A          := by apply_instance-/
+  （乘法结合律） mul_assoc a b c : a * b * c = a * (b * c)  -/
 
 universes u v
 variables {G : Type u} {H : Type v}
 
-class special (G: Type u) [group G] : Prop := (haha: ∀ x y: G, x*y=y*x)
+class ab (G: Type u) [group G] : Prop := (ab: ∀ x y: G, x*y=y*x)
 
-def ab [group G] := ∀ x y: G, x*y=y*x
-
-example [group G] [sg: special G] (x y: G): x*y=y*x :=
+example [group G] [h: ab G] (x y: G): x*y=y*x :=
 begin
-  
+  rw h.ab,
 end
+
+
+def hab [group G] (x y: G) := x*y=y*x
+
+example [group G] (x y: G) (h: hab x y): x*y=y*x :=
+begin
+  have h: x*y=y*x,
+    exact h,
+  rw h,
+end
+
+def h1 [group G] (x y: G) := y*x=x*x*y
+
+
+local attribute [simp] mul_assoc
+
+example [gg: group G] (x y: G) [h: h1 x y]: x*y*x*y=x^3*y^2 :=
+begin
+  have h1: y*x=x*x*y,
+    exact h,
+  repeat{rw mul_assoc, rw mul_assoc, try{rw h1}},
+  
+  sorry,
+  
+
+end
+
+
+
+example (x y z w: ℝ): x * y * z * w = x * ((y * z) * w) :=
+begin
+  rw mul_assoc,
+  rw ← mul_assoc,
+  
+
+  repeat{rw mul_assoc,},
+end
+
+
 
